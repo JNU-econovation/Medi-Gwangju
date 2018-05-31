ad25fea5de04bdf1ea0e0b2fb7789931e81c7624
조은솔 <eunsol@jo-eunsol-ui-macbook-pro.local>
2018-06-01 01:10:05 +0900

    Initial Commit

---
+++ MedicalGwangju/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MedicalGwangju
+//
+//  Created by 조은솔 on 2018. 6. 1..
+//  Copyright © 2018년 sol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
