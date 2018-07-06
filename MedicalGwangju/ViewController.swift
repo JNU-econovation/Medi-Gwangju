d92054fc107af2d8f0e2807b6ac4d5ee36c239b3
Cory Kim <corykim0829@gmail.com>
2018-07-06 22:07:06 +0900

    Button UI Border/Shadow

--- MedicalGwangju/ViewController.swift
+++ MedicalGwangju/ViewController.swift
@@ -8,15 +8,39 @@ class ViewController: UIViewController, XMLParserDelegate{
     var blank:Bool=true
     @IBOutlet var waittingBar: UIActivityIndicatorView!
     
+    @IBOutlet weak var searchButton: UIButton!
     @IBOutlet weak var foreignStudentButton: UIButton!
     @IBOutlet weak var interExchangeCenterButton: UIButton!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Dongjun's UI code Start
+        searchButton.layer.cornerRadius = 10
+        searchButton.layer.borderWidth = 0.0/UIScreen.main.nativeScale
+        
+        // 유학생이세요?
         foreignStudentButton.layer.cornerRadius = 10
-        foreignStudentButton.layer.borderWidth = 1/UIScreen.main.nativeScale
+        foreignStudentButton.layer.borderWidth = 0.0/UIScreen.main.nativeScale
+        
+        foreignStudentButton.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.5).cgColor
+        foreignStudentButton.layer.shadowOffset = CGSize(width: 3.0, height: 3.0)
+        foreignStudentButton.layer.shadowOpacity = 0.2
+        foreignStudentButton.layer.shadowRadius = 3.0
+        foreignStudentButton.layer.masksToBounds = false
+        
+        // 광주국제교류센터
+        interExchangeCenterButton.layer.cornerRadius = 10
+        interExchangeCenterButton.layer.borderWidth = 0.0/UIScreen.main.nativeScale
+        
+        interExchangeCenterButton.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.5).cgColor
+        interExchangeCenterButton.layer.shadowOffset = CGSize(width: 3.0, height: 3.0)
+        interExchangeCenterButton.layer.shadowOpacity = 0.2
+        interExchangeCenterButton.layer.shadowRadius = 3.0
+        interExchangeCenterButton.layer.masksToBounds = false
+        
         // Dongjun's UI code End
         
     }
